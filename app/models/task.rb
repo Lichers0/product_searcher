7b4076a3e76c8b22ed3a82404d250de1009b5948
Denis Romodanov <romodanov.denis@gmail.com>
2021-04-23 16:01:17 +0500

    modify services

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,18 +5,19 @@ class Task < ApplicationRecord
 
   validates :email, :seller_id, :mws_auth_token, :ship_to_fba, :services_cost, presence: true
   validates :file, attached: true
-  validate  :csv_header_errors?, :api_errors?
+  validate  :price_columns_presence, :amazon_user_api_keys
 
   private
 
-  def api_errors?
-    errors.add(:wrong_keys, "amazon keys errors") if ApiChecker.call(self).error?
+  def amazon_user_api_keys
+    errors.add(:wrong_keys, "amazon keys errors") if AmazonUserApiKeys.new(seller_id, mws_auth_token).invalid?
   end
 
-  def csv_header_errors?
+  def price_columns_presence
     return true if file.blank?
 
     temp_file = attachment_changes["file"].attachable
-    errors.add(:wrong_csv_header, "don't find columns") if CsvHeaderChecker.call(temp_file).error?
+    binding.pry
+    errors.add(:wrong_csv_header, "don't find columns") if Price.new(temp_file).import_columns_presence?
   end
 end
