ebc909ae02fc0562fba873ccfd916d028b1fd64b
Denis Romodanov <romodanov.denis@gmail.com>
2021-06-09 14:31:18 +0500

    VCR setup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -6,4 +6,6 @@ VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = true
   c.cassette_library_dir = "spec/vcr_cassettes"
   c.hook_into :webmock
+
+  c.configure_rspec_metadata!
 end
