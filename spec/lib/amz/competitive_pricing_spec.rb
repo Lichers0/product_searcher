7376b9e0c6dc059cbba1a47e34fbb0808ecbd0d5
Denis Romodanov <romodanov.denis@gmail.com>
2021-06-10 17:14:29 +0500

    fix

--- spec/lib/amz/competitive_pricing_spec.rb
+++ spec/lib/amz/competitive_pricing_spec.rb
@@ -7,10 +7,8 @@ RSpec.describe Amz::CompetitivePricing do
     VCR.use_cassette "amz/competitive_pricing" do
       asin = "B00BUFXI6M"
       price_product = 4.65
-      task = build(:task)
-      params = task.api_keys.merge(marketplace: marketplace_us)
 
-      result = described_class.new(params).for_asin(asin: asin)
+      result = described_class.new(api_keys_and_marketplace).for_asin(asin: asin)
 
       expect(result.landed_price).to eq price_product
     end
@@ -20,13 +18,17 @@ RSpec.describe Amz::CompetitivePricing do
     VCR.use_cassette "amz/competitive_pricing" do
       asin = "B00BUFXI6M"
       offers_count = 8
-      task = build(:task)
-      params = task.api_keys.merge(marketplace: marketplace_us)
 
-      result = described_class.new(params).for_asin(asin: asin)
+      result = described_class.new(api_keys_and_marketplace).for_asin(asin: asin)
 
       expect(result.offers_count).to eq offers_count
     end
+  end
+
+  def api_keys_and_marketplace
+    attributes_for(:task)
+      .slice(:seller_id, :mws_auth_token)
+      .merge(marketplace: marketplace_us)
   end
 
   def marketplace_us
