fff629bb0f5d605122cc9cbc7caab04caf781822
Denis Romodanov <romodanov.denis@gmail.com>
2021-06-16 17:44:49 +0500

    last fix

--- spec/lib/amz/competitive_pricing_spec.rb
+++ spec/lib/amz/competitive_pricing_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe Amz::CompetitivePricing do
   describe "#product_price" do
-    it "returns product price " do
+    it "returns product price" do
       VCR.use_cassette "amz/competitive_pricing" do
         asin = "B00BUFXI6M"
         product_price = 4.65
