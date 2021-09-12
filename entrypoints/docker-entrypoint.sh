f369abba21c1b7629f34c4b21e2fa0132354cb82
Denis Romodanov <romodanov.denis@gmail.com>
2021-09-12 17:42:21 +0500

    ga fix entrypoints

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,6 +6,5 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-# bundle exec rails db:migrate
-# bundle exec rails s -b 0.0.0.0
-bash
+bundle exec rails db:migrate
+bundle exec rails s -b 0.0.0.0
