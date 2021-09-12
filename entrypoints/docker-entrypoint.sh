8b68a2c6b880fe9db4806805d6df2ee583189d66
Denis Romodanov <romodanov.denis@gmail.com>
2021-09-12 16:40:36 +0500

    test

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,5 +6,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rails db:migrate
-bundle exec rails s -b 0.0.0.0
+# bundle exec rails db:migrate
+# bundle exec rails s -b 0.0.0.0
+bash
